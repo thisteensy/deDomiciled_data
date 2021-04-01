bf3e20bff1c846bb88f28e07de9fccc106790246
Tina Haiser <tinahaiser@gmail.com>
2021-04-01 03:09:10 +0000

    creat json file working

--- csv_reader.py
+++ csv_reader.py
@@ -15,11 +15,9 @@ def csv_to_dictlist():
                     year = int(filename[:4])
                     state_abr = row["CoC"][:2]
                     count = int(row["Count"])
-                    # if homeless_count_by_state = []:
-                    #     homeless_count_by_state.append({'state_id' : state_abr,
-                    #                                       'data_year' : year, 
-                    #                                       'pit_count' : count})
+                
                     filtered_list = [i for i in homeless_count_by_state if i.get('state_id') == state_abr and i.get('data_year') == year]                                   
+                    
                     if filtered_list:
                         filtered_list[0]['pit_count'] += count
                     
